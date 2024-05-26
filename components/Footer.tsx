b5ddd33d40461ac803b1826e7b696de46b91227b
Aryan Biswas <aryanb198@gmail.com>
2024-05-26 14:18:49 +0530

    Updated Changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -44,7 +44,7 @@ const Footer = () => {
                 <span className='text-purple'>Aryan Biswas</span>
             </p>
 
-            <div className='flex items-center md:gap-4 gap-5'>
+            <div className='flex items-center my-3 md:my-0 md:gap-4 gap-5'>
                 {socialMedia.map((profile) => (
                     <div key={profile.id} className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300' onClick={() => handleClick(profile?.link)}>
                         <img src={profile.img} alt={profile.id} width={20} height={20} />
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,8 +3,10 @@ import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 import MagicButton from "./ui/MagicButton";
 import { FaLocationArrow } from "react-icons/fa6";
+import { FlipWords } from "./ui/FlipWords";
 
 const Hero = () => {
+  const words = ["Flexible", "Dynamic", "Modern", "Versatile", "Seamless", "Scalable"];
   return (
     <div className="pb-20 pt-36">
       <div>
@@ -24,30 +26,38 @@ const Hero = () => {
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>
 
-
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-            <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-                Dynamic Web Magic with Next.js
-            </h2>
+          <h2 className="uppercase tracking-widest text-xs my-2 text-center text-blue-100 max-w-80">
+            Dynamic Web Magic with Next.js
+          </h2>
 
-            <TextGenerateEffect 
-                className="text-center text-[40px] md:text-5xl lg:text-6xl"
-                words="Transforming Ideas into Flawless User Experiences"
+          <div>
+            <TextGenerateEffect
+              className="text-center text-[40px] md:text-5xl lg:text-6xl"
+              words="Transforming Ideas into "
             />
 
-            <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl ">
-                Hi, I&apos;m Aryan, a Software Developer based in India.
-            </p>
+            <FlipWords className="text-center text-[40px] md:text-5xl lg:text-6xl" words={words} />
 
-            <a href="#about">
-                <MagicButton
-                    title="Show my work"
-                    icon={<FaLocationArrow />}
-                    position='right'
-                    otherClasses="tracking-wide text-md"
-                />
-            </a>
+            <TextGenerateEffect
+              className="text-center text-[40px] md:text-5xl lg:text-6xl"
+              words="User Experiences"
+            />
+          </div>
+
+          <p className="text-center md:tracking-wider my-4 mt-3 text-sm md:text-lg lg:text-2xl ">
+            Hi, I&apos;m Aryan, a Software Developer based in India.
+          </p>
+
+          <a href="#about">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+              otherClasses="tracking-wide text-md"
+            />
+          </a>
         </div>
       </div>
     </div>
